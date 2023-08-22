b2708bb74ba86e25bc8f0898d16eee9e4b5b7d8b
zhangxiaoyuan <1666888816@qq.com>
2023-08-22 09:55:56 +0800

    nuxt-cli

---
+++ pages/app/[id].vue
@@ -0,0 +1,29 @@
+<script setup lang="js">
+useHead({
+  title: 'My App',
+  meta: [
+    { name: 'description', content: 'My amazing site.' }
+  ],
+  bodyAttrs: {
+    class: 'test'
+  }
+})
+const getdata = ( x )=>{
+  console.log(x)
+}
+const data2 = ref(1)
+const appId = useRoute().params.id
+console.log(appId)
+
+const {data: res} = await useFetch('https://hub.grapps.cn/app-server/marketui/859a51f9bb3b48b5bfd222e3bef56425/apps/' + appId + '/detail')
+
+</script>
+
+<template>
+  <div v-html="res.desc">
+  </div>
+</template>
+
+<style scoped>
+
+</style>
